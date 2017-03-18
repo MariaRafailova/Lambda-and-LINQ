b7a61ad1c98d47148275981ab3cc02396c499588
MariaRafailova <mrafailova@gmail.com>
2017-03-18 18:43:46 +0200

    Add files via upload

---
+++ LambadaExpressions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambadaExpressions
+{
+    public class LambadaExpressions
+    {
+        private static object selector;
+
+        public static void Main()
+        {
+            var lambadaExpressions = new Dictionary<string, Dictionary<string, string>>();
+
+            var inputLine = Console.ReadLine();
+
+            while (inputLine != "lambada")
+            {
+                string[] inputParams = inputLine.Split(new[] { ' ', '=', '>', '.' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (inputParams.Length > 1)
+                {
+                    string selector = inputParams[0];
+                    string selectorObject = inputParams[1];
+                    string property = inputParams[2];
+
+                    if (!lambadaExpressions.ContainsKey(selector))
+                    {
+                        lambadaExpressions.Add(selector, new Dictionary<string, string>());
+                    }
+
+                    lambadaExpressions[selector][selectorObject] = property;
+                }
+                else
+                {
+                    lambadaExpressions = lambadaExpressions
+                    .ToDictionary(selector => selector.Key, selector => selector.Value.
+                    ToDictionary(selectorObject => selectorObject.Key,
+                    selectorObject => selectorObject.Key + "." + selectorObject.Value));
+                }
+
+                inputLine = Console.ReadLine();
+            }
+
+            lambadaExpressions
+                .ToList()
+                .ForEach(selector => selector.Value
+                .ToList()
+                .ForEach(selectorObject => Console.WriteLine("{0} => {1}.{2}",
+                selector.Key, selectorObject.Key, selectorObject.Value)));
+        }
+    }
+}
---
+++ OrderedBankingSystem.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderedBankingSystem
+{
+    public class OrderedBankingSystem
+    {
+        public static void Main()
+        {
+            var banksAccounts = new Dictionary<string, Dictionary<string, decimal>>();
+
+            string inputLine = Console.ReadLine();
+
+            while (inputLine != "end")
+            {
+                string[] inputParams = inputLine.Split(new[] { ' ', '-', '>' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string bankName = inputParams[0];
+                string bankAccountName = inputParams[1];
+                decimal bankAccountBalance = decimal.Parse(inputParams[2]);
+
+                if (!banksAccounts.ContainsKey(bankName))
+                {
+                    banksAccounts.Add(bankName, new Dictionary<string, decimal>());
+                }
+
+                if (!banksAccounts[bankName].ContainsKey(bankAccountName))
+                {
+                    banksAccounts[bankName].Add(bankAccountName, 0);
+                }
+
+                banksAccounts[bankName][bankAccountName] += bankAccountBalance;
+
+                inputLine = Console.ReadLine();
+            }
+
+            foreach (var bank in banksAccounts.OrderByDescending(bank => bank.Value.Sum(account => account.Value))
+            .ThenByDescending(bank => bank.Value.Max(account => account.Value)))
+            {
+                foreach (var account in bank.Value.OrderByDescending(account => account.Value))
+                {
+                    Console.WriteLine("{1} -> {2} ({0})", bank.Key, account.Key, account.Value);
+                }
+            }
+
+            //banksAccounts
+            //    .OrderByDescending(bank => bank.Value.Sum(account => account.Value))
+            //    .ThenByDescending(bank => bank.Value.Max(account => account.Value))
+            //    .ToList()
+            //    .ForEach(bank => bank.Value
+            //    .OrderByDescending(account => account.Value)
+            //    .ToList()
+            //    .ForEach(account => Console.WriteLine("{0} -> {1} ({2})", account.Key, account.Value, bank.Key)));
+        }
+    }
+}
